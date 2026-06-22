aea7408f5fc9f0a3fb61ac32e99c831e1d731870
Natsuki Iida <natsuki.iida0511@gmail.com>
2026-06-22 13:17:47 +0900

    configure netlify proxy

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "/api",
+    //baseURL: import.meta.env.VITE_API_URL,
     timeout: 5000,
     headers: {'Content-type': 'application/json'}
 });
