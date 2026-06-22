accf3f04dc1eea4fc33cc3e4fdaec0b0598cc1a7
Natsuki Iida <natsuki.iida0511@gmail.com>
2026-06-22 11:27:56 +0900

    Add environmet variables

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: import.meta.env.VITE_API_URL,
     timeout: 5000,
     headers: {'Content-type': 'application/json'}
 });
